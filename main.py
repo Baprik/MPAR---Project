0a4187c984bdac0f02379914820d9258f1b775ed
Baptiste <baptiste.boutauddlc@gmail.com>
2024-03-06 17:11:37 +0100

    ajout Update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from mdp import gramPrintListener
 
 def main():
     #lexer = gramLexer(StdinStream())
-    lexer = gramLexer(FileStream("ex2.mdp")) #pour éviter d'écrirer le < ex.mdp
+    lexer = gramLexer(FileStream("simu-mdp.mdp")) #pour éviter d'écrirer le < ex.mdp
     stream = CommonTokenStream(lexer)
     parser = gramParser(stream)
     tree = parser.program()
